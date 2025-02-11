fbcafd961eddc9345ff2868d3f5145ab7a5e6fc0
Seongho Kim <kksshh0612@gmail.com>
2025-02-11 15:13:35 +0900

    feat : 초기 환경 설정

---
+++ src/main/java/org/example/petsystem/PetSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.petsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetSystemApplication.class, args);
+    }
+
+}
