8646d9f97c0936dcaf433dc9a33c538e40a632e4
Seongho Kim <kksshh0612@gmail.com>
2025-02-11 16:11:13 +0900

    feat : 회원 도메인 설계

---
+++ src/main/java/org/example/petsystem/domain/member/Member.java
@@ -0,0 +1,43 @@
+package org.example.petsystem.domain.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    private String emailAddress;
+
+    private String password;
+
+    private String name;
+
+    private String phoneNumber;
+
+    private MemberRole memberRole;
+
+    private boolean activeStatus;
+
+    @Builder
+    public Member(String emailAddress, String password, String name, String phoneNumber, MemberRole memberRole,
+                  boolean activeStatus) {
+        this.emailAddress = emailAddress;
+        this.password = password;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.memberRole = memberRole;
+        this.activeStatus = activeStatus;
+    }
+}
