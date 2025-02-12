7ff508601c6d99c64a5595e9aaf7ff7f49f175a1
Seongho Kim <kksshh0612@gmail.com>
2025-02-12 16:27:48 +0900

    feat : swagger 설정 완료

---
+++ src/main/java/org/example/petsystem/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package org.example.petsystem.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+
+        Info info = new Info()
+                .title("PET CARE SYSTEM API Document")
+                .description("반려동물 케이 서비스 프로젝트의 API 명세서");
+
+        return new OpenAPI()
+                .info(info);
+    }
+}
