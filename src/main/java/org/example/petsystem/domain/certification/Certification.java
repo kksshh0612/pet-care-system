9c0b1015603693757d3d955c48cf9af9ee87836e
Seongho Kim <kksshh0612@gmail.com>
2025-02-14 10:30:43 +0900

    feat : 인증서 도메인 설계

--- src/main/java/org/example/petsystem/domain/certification/Certification.java
+++ src/main/java/org/example/petsystem/domain/certification/Certification.java
@@ -1,27 +1,29 @@
-package org.example.petsystem.domain.file;
+package org.example.petsystem.domain.certification;
 
-import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.example.petsystem.domain.certification.Certification;
 import org.example.petsystem.domain.member.Member;
 
 @Entity
 @Getter
-@DiscriminatorValue("certi")
 @NoArgsConstructor
-public class CertificationFile extends File {
+public class Certification {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "certification_id")
+    private Long id;
+
+    private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "certification_id")
-    private Certification certification;
 }
