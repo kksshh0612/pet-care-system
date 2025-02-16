9fc3fab2df413aec4823d3ae0bc7e89e8656c75d
Seongho Kim <kksshh0612@gmail.com>
2025-02-16 22:46:05 +0900

    feat : 증명서 파일은 Member가 아니라 PetSitter와 연관관계를 맺음

--- src/main/java/org/example/petsystem/domain/file/CertificationFile.java
+++ src/main/java/org/example/petsystem/domain/file/CertificationFile.java
@@ -6,10 +6,12 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.petsystem.domain.certification.Certification;
 import org.example.petsystem.domain.member.Member;
+import org.example.petsystem.domain.petsitter.PetSitter;
 
 @Entity
 @Getter
@@ -18,10 +20,17 @@ import org.example.petsystem.domain.member.Member;
 public class CertificationFile extends File {
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
+    @JoinColumn(name = "pet_sitter_id")
+    private PetSitter petSitter;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "certification_id")
     private Certification certification;
+
+    @Builder
+    public CertificationFile(String url, PetSitter petSitter, Certification certification) {
+        super(url);
+        this.petSitter = petSitter;
+        this.certification = certification;
+    }
 }
--- src/main/java/org/example/petsystem/repository/certification/CertificationRepository.java
+++ src/main/java/org/example/petsystem/repository/certification/CertificationRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CertificationRepository extends JpaRepository<Certification, Long> {
 
-    List<Certification> findAllByMember(Member member);
 }
